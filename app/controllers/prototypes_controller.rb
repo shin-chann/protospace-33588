347a487998790e5c5b1de2afa18e637c2b334855
shinpei-nakayama <wtpmjgd.a@icloud.com>
2021-01-20 13:00:49 +0900

    新規投稿をしようとするとエラーが出る修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_prototype, only: [:new, :edit, :update, :destroy]
+  before_action :set_prototype, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.all
   end
